85fd353ca131f380f6a494c278a2196a854b1893
Iggy <igoririanto@rocketmail.com>
2018-12-25 14:28:50 -0800

    add command pattern

---
+++ command-pattern/command-pattern-example.js
@@ -0,0 +1,27 @@
+(function(){
+  var carManager = {
+    requestInfo: function(model,id){
+      return "The information for " + model + " with ID " + id + " is foobar";
+    },
+
+    buyVehicle: function(model, id){
+      return "You have successfully purchased item " + id + ", a " + model;
+    },
+
+    arrangeViewing: function(model, id){
+      return "You have successfully booked a viewing of " + model + " ( " + id + " )";
+    }
+  }
+})();
+
+/* 
+
+carManager.execute = function ( name ) {
+    return carManager[name] && carManager[name].apply( carManager, [].slice.call(arguments, 1) );
+};
+
+carManager.execute( "arrangeViewing", "Ferrari", "14523" );
+carManager.execute( "requestInfo", "Ford Mondeo", "54323" );
+carManager.execute( "requestInfo", "Ford Escort", "34232" );
+carManager.execute( "buyVehicle", "Ford Escort", "34232" );
+*/
