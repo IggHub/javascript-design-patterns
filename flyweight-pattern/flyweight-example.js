ddacddaa7b093d5d7adb1b9f133c7d3bc3cb7d03
Iggy <igoririanto@rocketmail.com>
2018-12-31 10:32:49 -0800

    add flyweight examples

--- flyweight-pattern/flyweight-example.js
+++ flyweight-pattern/flyweight-example.js
@@ -203,7 +203,45 @@ var BookFactory = (function () {
   };
  
 })();
-BookFactory.createBook("some title", "some author", 100, "111-1111", "12345", "firstISBN");
-BookFactory.displayBooks();
-BookFactory.createBook("some other title", "some other author", 300, "333-3333", "11111", "secondISBN")
-BookFactory.displayBooks();
+//  BookFactory.createBook("some title", "some author", 100, "111-1111", "12345", "firstISBN");
+//  BookFactory.displayBooks();
+//  BookFactory.createBook("some other title", "some other author", 300, "333-3333", "11111", "secondISBN")
+//  BookFactory.displayBooks();
+var BookRecordManager = (function () {
+ 
+  var bookRecordDatabase = {};
+ 
+  return {
+    // add a new book into the library system
+    addBookRecord: function ( id, title, author, genre, pageCount, publisherID, ISBN, checkoutDate, checkoutMember, dueReturnDate, availability ) {
+ 
+      var book = BookFactory.createBook( title, author, genre, pageCount, publisherID, ISBN );
+ 
+      bookRecordDatabase[id] = {
+        checkoutMember: checkoutMember,
+        checkoutDate: checkoutDate,
+        dueReturnDate: dueReturnDate,
+        availability: availability,
+        book: book // book is the new Book object (returned from BookFactory.createBook())
+      };
+    },
+    updateCheckoutStatus: function ( bookID, newStatus, checkoutDate, checkoutMember, newReturnDate ) {
+ 
+      var record = bookRecordDatabase[bookID];
+      record.availability = newStatus;
+      record.checkoutDate = checkoutDate;
+      record.checkoutMember = checkoutMember;
+      record.dueReturnDate = newReturnDate;
+    },
+ 
+    extendCheckoutPeriod: function ( bookID, newReturnDate ) {
+      bookRecordDatabase[bookID].dueReturnDate = newReturnDate;
+    },
+ 
+    isPastDue: function ( bookID ) {
+      var currentDate = new Date();
+      return currentDate.getTime() > Date.parse( bookRecordDatabase[bookID].dueReturnDate );
+    }
+  };
+ 
+})();
